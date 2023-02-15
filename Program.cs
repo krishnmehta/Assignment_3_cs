687cb59c78de3018ed783d2ade3feaf3a8adc4cb
krishnmehta <87222503+krishnmehta@users.noreply.github.com>
2023-02-15 18:07:01 +0530

    Assignment - 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Assignment3
 {
@@ -7,17 +8,17 @@ namespace Assignment3
         static void Main(string[] args)
         {
             //Creating Dictionary with key = integer and value = string
-            Dictionary<int, string> Primeminister  = new Dictionary<int, string>();
+            Dictionary<int, string> Primeminister = new Dictionary<int, string>();
 
             //Adding elements in dictionary
-            Primeminister.Add(1998,"Atal Bihari Vajpayee");
+            Primeminister.Add(1998, "Atal Bihari Vajpayee");
             Primeminister.Add(2014, "Narendra Modi");
             Primeminister.Add(2004, "Manmohan Singh");
 
             //Find the prime minister of 2004
-            if(Primeminister.TryGetValue(2004,out string value))
+            if (Primeminister.TryGetValue(2004, out string value))
             {
-                Console.WriteLine("Prime Minister of the year 2004 is: "+value);
+                Console.WriteLine("Prime Minister of the year 2004 is: " + value);
             }
             else
             {
@@ -30,12 +31,12 @@ namespace Assignment3
             //Sorting the dictionary by year
             Console.WriteLine("\nSorted by Year");
             Console.WriteLine("=============");
-  
+
             var keys_sorted = Primeminister.Keys.ToList();
             keys_sorted.Sort();
-            foreach(var k in keys_sorted)
+            foreach (var k in keys_sorted)
             {
-                Console.WriteLine( k+ " "+Primeminister[k]);
+                Console.WriteLine(k + " " + Primeminister[k]);
             }
             Console.WriteLine("=============");
         }
